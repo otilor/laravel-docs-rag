d2b102ecf7b0f4400d526fd6a44264e513aa2acf
Gabriel <gabrielfemi799@gmail.com>
2026-04-01 07:42:07 +0100

    use ollama to make embeddings

---
+++ main.py
@@ -0,0 +1,49 @@
+import getpass
+import os
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.embeddings import DeterministicFakeEmbedding
+from langchain_ollama import OllamaEmbeddings
+
+
+os.environ["LANGSMITH_tracing"] = "true"
+os.environ["LANGSMITH_API_KEY"] = getpass.getpass()
+
+#load and chunk contents of the blog
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/", ),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    )
+)
+
+docs = loader.load()
+
+embeddings = OllamaEmbeddings(model="llama3.1")
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+vector_1 = embeddings.embed_query(all_splits[0].page_content)
+
+print(vector_1)
+exit
+
+vector_store= Chroma(
+    embedding_function=embeddings,
+    persist_directory='my_file_db',
+    collection_name='sample',
+)
+# Index chunks
+_ = vector_store.add_documents(documents=all_splits)
+
+# Construct a tool for retrieving context
+@bool(response_format="content_and_artifact")
+def retrieve_context(query: str):
+    pass
